7d404121506c2a69ad1da7012e3b7598d68a44d4
MartinM <martin.meltsas@fortumo.com>
2019-09-20 15:55:21 +0300

    ask user permission before modifying tslint.json

--- modify-tslint.js
+++ modify-tslint.js
@@ -2,14 +2,41 @@ const fs = require('fs')
 
 const path = '../../tslint.json'
 
+const cl = require('readline').createInterface(process.stdin, process.stdout);
+const question = function (q) {
+  return new Promise((res, rej) => {
+    cl.question(q, answer => {
+      res(answer);
+    })
+  });
+};
+
+
 try {
-  const content = fs.readFileSync('./tslint.json');
+  const content = fs.readFileSync('./mm-tslint.json');
   if (fs.existsSync(path)) {
-    //file exists
+    (async function main() {
+      const userAnswers = ['yes', 'y', 'no', 'n'];
+      let answer = '';
+      while (userAnswers.indexOf(answer.toLowerCase()) === -1) {
+        answer = await question('Do You want to modify Your tslint.json with mm-tslint rules? (Y/N): ');
+      }
+      if (answer === 'yes' || answer === 'y') {
+        fs.writeFileSync(path, content, {encoding: 'utf8', flag: 'w'});
+        console.info('Your tslint were modified with mm-tslint rules');
+        process.exit();
+      } else {
+        console.info('Your tslint were not modified !');
+        process.exit();
+      }
+    })();
+
     fs.writeFileSync(path, content, {encoding: 'utf8', flag: 'w'});
+
   } else {
     console.error('No tsling.json file found in Your project root folder!');
   }
 } catch (err) {
   console.error('error', err);
 }
+
